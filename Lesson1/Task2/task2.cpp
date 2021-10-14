6b775fd7a3d1d9817274658843ebbe7c32098b17
unknown <vasylenko939@gmail.com>
2021-10-14 13:00:34 +0300

    task2

---
+++ Lesson1/Task2/task2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "_______________ " << endl
+         << "|  ____|_____  |" << endl
+         << "| |    |    |  |" << endl
+         << "| |----|----|  |" << endl
+         << "| |____|____|  |" << endl
+         << "|______|_______|" << endl;
+
+
+    return 0;
+}
