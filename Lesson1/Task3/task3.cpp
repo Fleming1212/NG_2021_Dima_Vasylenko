0d9baf84838f185fa370481b72eaddb5edf2aead
unknown <vasylenko939@gmail.com>
2021-10-14 12:39:55 +0300

    task3(changed)

--- Lesson1/Task3/task3.cpp
+++ Lesson1/Task3/task3.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main()
 {
-   int a=50;
-   int b=20;
-   a=a-b;
-   b=b+a;
-   a=b-a;
+    int a=50;
+    int b=20;
+
+    a=a-b;
+    b=b+a;
+    a=b-a;
 
     cout << a << endl;
-    cout << b   ;
+    cout << b;
 
 }
