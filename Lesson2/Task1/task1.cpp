3bd9d5a7dada767e80fc2ff410417c1d9194ff4c
unknown <vasylenko939@gmail.com>
2021-10-24 13:55:21 +0300

    added task 1,3

---
+++ Lesson2/Task1/task1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 0;
+    int result = 0;
+
+    cout << "Vvedite chislo: ";
+    cin >> num;
+
+    for (int i = 0; num > 0; i++){
+        result += num%10;
+        num/=10;
+
+    }
+    cout << result;
+
+}
