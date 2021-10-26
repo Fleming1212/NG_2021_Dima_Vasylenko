7001785b43fa72bee0d83757471f8e2ebb5b92e6
unknown <vasylenko939@gmail.com>
2021-10-26 23:34:19 +0300

    added task 5

---
+++ Lesson1/Task5/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    cout << "Space traveler" << endl;
+
+    int massa = 0;
+    int fuel = 0;
+
+    cout << "massa: ";
+    cin >> massa;
+    fuel = round (massa / 3) - 2;
+    cout << fuel;
+
+
+
+
+
+
+
+
+
+
+
+
+}
