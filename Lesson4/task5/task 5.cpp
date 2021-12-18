3726aae7503c0a06587b6da3623f3ed6c6803653
unknown <vasylenko939@gmail.com>
2021-12-18 20:20:01 +0200

    Added lesson 4 task 5

---
+++ Lesson4/task5/task 5.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    char words [100] = "";
+    cin.getline(words, 100);
+    for (int i = 0;words[i] != 0; i++)
+    {
+        if (words[i] == 'f' && words[i+1] == 'u' &&
+            words[i+2] == 'c' && words[i+3] == 'k')
+        {
+                words[i] = '*';
+                words[i+1] = '*';
+                words[i+2] = '*';
+                words[i+3] = '*';
+        }
+        cout << words[i];
+    }
+    return 0;
+}
